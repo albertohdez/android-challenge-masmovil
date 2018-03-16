996e98ea639353a4e89ec71ff7851f1dab9ed152
Alberto Hernandez <albertohernandez@iMac-2.local>
2018-03-16 16:58:53 +0100

    Basic webview login approximation

---
+++ app/src/main/java/alberto/masmovilchallenge/ImgurUser.java
@@ -0,0 +1,57 @@
+package alberto.masmovilchallenge;
+
+public class ImgurUser {
+    private int mId;
+    private String mUsername;
+    private String mAccessToken;
+    private String mRefreshToken;
+    private long mAccessTokenExpiration;
+
+
+    public ImgurUser(String username, String accessToken, String refreshToken, long accessTokenExpiration) {
+        mUsername = username;
+        mAccessToken = accessToken;
+        mRefreshToken = refreshToken;
+        mAccessTokenExpiration = accessTokenExpiration;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getmUsername() {
+        return mUsername;
+    }
+
+    public void setmUsername(String mUsername) {
+        this.mUsername = mUsername;
+    }
+
+    public String getmAccessToken() {
+        return mAccessToken;
+    }
+
+    public void setmAccessToken(String mAccessToken) {
+        this.mAccessToken = mAccessToken;
+    }
+
+    public String getmRefreshToken() {
+        return mRefreshToken;
+    }
+
+    public void setmRefreshToken(String mRefreshToken) {
+        this.mRefreshToken = mRefreshToken;
+    }
+
+    public long getmAccessTokenExpiration() {
+        return mAccessTokenExpiration;
+    }
+
+    public void setmAccessTokenExpiration(long mAccessTokenExpiration) {
+        this.mAccessTokenExpiration = mAccessTokenExpiration;
+    }
+}
--- app/src/main/java/alberto/masmovilchallenge/MainActivity.java
+++ app/src/main/java/alberto/masmovilchallenge/MainActivity.java
@@ -1,13 +1,92 @@
 package alberto.masmovilchallenge;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String CLIENT_ID = "ba6301daf01896f";
+    private static final String LOGIN_URL = "https://api.imgur.com/oauth2/authorize?client_id=" + CLIENT_ID + "&response_type=token";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configWebView();
+    }
+
+    private void configWebView() {
+        WebView webView = findViewById(R.id.loginWebView);
+        webView.loadUrl(LOGIN_URL);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.contains("/?error=")) {
+                    Log.v("MainActivity", "Error received from URL " + url);
+                    view.loadUrl(LOGIN_URL);
+                    return true;
+                }
+
+                // Extract the info from the callback url
+                String[] outerSplit = url.split("\\#")[1].split("\\&");
+                String username = null;
+                String accessToken = null;
+                String refreshToken = null;
+                long accessTokenExpiration = 0;
+                int index = 0;
+
+                for (String s : outerSplit) {
+                    String[] innerSplit = s.split("\\=");
+
+                    switch (index) {
+                        // Access Token
+                        case 0:
+                            accessToken = innerSplit[1];
+                            break;
+
+                        // Access Token Expiration
+                        case 1:
+                            long expiresIn = Long.parseLong(innerSplit[1]);
+                            accessTokenExpiration = System.currentTimeMillis() + (expiresIn * DateUtils.SECOND_IN_MILLIS);
+                            break;
+
+                        // Token Type, not using
+                        case 2:
+                            //NO OP
+                            break;
+
+                        // Refresh Token
+                        case 3:
+                            refreshToken = innerSplit[1];
+                            break;
+
+                        // Username
+                        case 4:
+                            username = innerSplit[1];
+                            break;
+                    }
+
+                    index++;
+                }
+
+                if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(accessToken) &&
+                        !TextUtils.isEmpty(refreshToken) && accessTokenExpiration > 0) {
+                    ImgurUser newUser = new ImgurUser(username, accessToken, refreshToken, accessTokenExpiration);
+
+                    CookieManager.getInstance().removeAllCookie();
+                    view.clearHistory();
+                    view.clearCache(true);
+                    view.clearFormData();
+                }
+
+                return true;
+            }
+        });
     }
 }
